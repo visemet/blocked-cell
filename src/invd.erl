3349341ed4d3113bc1947fb4bf3cfab8c1bf4d6e
Max Hirschhorn <maxh@caltech.edu>
2013-03-29 14:32:09 -0700

    Created get_state/1 function.
    When used in neighbors/2, has the potential for deadlock.

--- src/invd.erl
+++ src/invd.erl
@@ -1,7 +1,7 @@
 -module(invd).
 -behaviour(gen_server).
 
--export([start/3]).
+-export([start/3, get_state/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
@@ -27,20 +27,36 @@ start(Type, Args, Options) when is_atom(Type), is_list(Options) ->
     gen_server:start(?MODULE, [Type, Args, Options], [])
 .
 
+get_state(Invd) when is_pid(Invd) ->
+    gen_server:call(Invd, {get_state})
+.
+
 %% ----------------------------------------------------------------- %%
 
 init([Type, Args, Options]) when is_atom(Type), is_list(Options) ->
-    Genome = Type:init(Args)
+    case Type:init(Args) of
+        {ok, Genome} ->
+            init(Options, #invd{type=Type, genome=Genome})
 
-  , init(Options, #invd{type=Type, genome=Genome})
+      ; {error, Reason} ->
+            {error, Reason}
+    end
 .
+
+handle_call({get_state}, _From, State = #invd{}) ->
+    {reply, State, State}
+;
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}
 .
 
-handle_cast({evolve}, State = #invd{}) ->
+handle_cast({evolve}, State = #invd{ga = GA, index = Index}) ->
     pass %TODO:implement
+
+  , io:format("neighbors ~p~n", [neighbors(GA, Index)])
+
+  , {noreply, State}
 ;
 
 handle_cast(_Request, State) ->
@@ -86,6 +102,21 @@ init([{index, Index = {Row, Column}} | Options], State = #invd{})
 
 init([Term | _Options], #invd{}) ->
     {error, {badarg, Term}}
+.
+
+neighbors(GA, Index = {Row, Column})
+  when
+    is_pid(GA)
+  , is_integer(Row), Row >= 0
+  , is_integer(Column), Column >= 0
+  ->
+    lists:map(
+        fun (Invd) when is_pid(Invd) ->
+            invd:get_state(Invd) % this could cause deadlock
+        end
+
+      , ga:neighbors(GA, Index)
+    )
 .
 
 send_genome(SenderInvd, ReceiverInvd) ->
