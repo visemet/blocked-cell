8a611ff1d723ddbcd415c09033ffc8157fd07157
Max Hirschhorn <maxh@caltech.edu>
2013-03-29 20:52:49 -0700

    Created linear5_ga module.
    Implements genetic algorithm behaviour. Uses the von Neumann
    neighborhood.

---
+++ src/linear5_ga.erl
@@ -0,0 +1,55 @@
+-module(linear5_ga).
+-behaviour(ga).
+
+-export([start/3]).
+-export([get_neighbor_coords/2]).
+
+%%% =============================================================== %%%
+%%%  API                                                            %%%
+%%% =============================================================== %%%
+
+start(Size = {NRows, NCols}, InvdType, InvdArgs)
+  when
+    is_integer(NRows), NRows >= 0
+  , is_integer(NCols), NCols >= 0
+  , is_atom(InvdType)
+  ->
+    ga:start(Size, ?MODULE, InvdType, InvdArgs)
+.
+
+%% ----------------------------------------------------------------- %%
+
+get_neighbor_coords(Index = {Row, Col}, _Size = {NRows, NCols})
+  when
+    is_integer(Row), Row >= 0
+  , is_integer(Col), Col >= 0
+  , is_integer(NRows), NRows >= 0, Row < NRows
+  , is_integer(NCols), NCols >= 0, Col < NCols
+  ->
+    North = {Row - 1, Col}
+  , East = {Row, Col + 1}
+  , South = {Row + 1, Col}
+  , West = {Row, Col - 1}
+
+  , lists:filter(
+        fun ({R, C}) when is_integer(R), is_integer(C) ->
+            if
+                R >= 0, R < NRows, C >= 0, C < NCols ->
+                  true
+
+              ; true ->
+                  false
+            end
+        end
+
+      , [Index, North, East, South, West]
+    )
+.
+
+%%% =============================================================== %%%
+%%%  private functions                                              %%%
+%%% =============================================================== %%%
+
+
+
+%% ----------------------------------------------------------------- %%
