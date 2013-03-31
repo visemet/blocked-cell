0f9293f963bbf4d233742a7ae69afea47dc43896
Max Hirschhorn <maxh@caltech.edu>
2013-03-31 14:12:37 -0700

    Implemented sat_invd:evaluate/1.
    Uses the following defintions as taken from De Jong (1989):
      (1) val(NOT e) = 1 - val(e),
      (2) val(AND e1 ... en) = AVE(val(e1) ... val(en)),
      (3) val(OR e1 ... en) = MAX(val(e1) ... val(en)),
    which provide a differential payoff property.
    
    Originally had idea to fully substitute the formula with all the
    variables. This may be done for ease of converting to a tail-recursive
    procedure.

--- src/invd/sat_invd.erl
+++ src/invd/sat_invd.erl
@@ -15,8 +15,9 @@ init(Args) ->
     init(Args, #sat{})
 .
 
-evaluate(_Genome = #sat{}) ->
-    -1
+evaluate(_Genome = #sat{vars = Vars, formula = Formula}) ->
+    % Substituted = substitute(dict:from_list(Vars), Formula)
+    evaluate(dict:from_list(Vars), Formula)
 .
 
 select([{Invd, _Fitness} | _Rest]) ->
@@ -96,6 +97,102 @@ init([{formula, Formula} | Args], State = #sat{}) when is_list(Args) ->
 
 init([Term, _Args], #sat{}) ->
     {error, {badarg, Term}}
+.
+
+%% ----------------------------------------------------------------- %%
+
+substitute(_Vars, 0) ->
+    0
+;
+
+substitute(_Vars, 1) ->
+    1
+;
+
+substitute(Vars, Literal) when is_atom(Literal) ->
+    dict:fetch(Literal, Vars)
+;
+
+substitute(Vars, Formula = #'and'{clauses = Clauses}) ->
+    NewClauses = lists:map(
+        fun (Clause) ->
+            substitute(Vars, Clause)
+        end
+
+      , Clauses
+    )
+
+  , Formula#'and'{clauses=NewClauses}
+;
+
+substitute(Vars, Formula = #'or'{clauses = Clauses}) ->
+    NewClauses = lists:map(
+        fun (Clause) ->
+            substitute(Vars, Clause)
+        end
+
+      , Clauses
+    )
+
+  , Formula#'or'{clauses=NewClauses}
+;
+
+substitute(Vars, Formula = #'not'{clause = Clause}) ->
+    NewClause = substitute(Vars, Clause)
+
+  , Formula#'not'{clause=NewClause}
+.
+
+%% ----------------------------------------------------------------- %%
+
+evaluate(_Vars, 0) ->
+    0
+;
+
+evaluate(_Vars, 1) ->
+    1
+;
+
+evaluate(Vars, Literal) when is_atom(Literal) ->
+    dict:fetch(Literal, Vars)
+;
+
+evaluate(Vars, #'and'{clauses = Clauses}) ->
+    Count = erlang:length(Clauses)
+
+  , Sum = lists:foldl(
+        fun (Clause, Acc) ->
+            Value = evaluate(Vars, Clause)
+
+          , Acc + Value
+        end
+
+      , 0
+      , Clauses
+    )
+
+  , Sum / Count
+;
+
+evaluate(Vars, #'or'{clauses = Clauses}) ->
+    lists:foldl(
+        fun (Clause, Acc) ->
+            Value = evaluate(Vars, Clause)
+
+          , if
+                Value > Acc -> Value
+
+              ; Value =< Acc -> Acc
+            end
+        end
+
+      , 0
+      , Clauses
+    )
+;
+
+evaluate(Vars, #'not'{clause = Clause}) ->
+    1 - evaluate(Vars, Clause)
 .
 
 %% ----------------------------------------------------------------- %%
